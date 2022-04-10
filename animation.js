05a6661da8e1314a9af78f7c5827f5ed6340598a
Hugues Mourice <mourice.hugues@gmail.com>
2022-04-10 18:51:19 +0200

    about-me section added

--- animation.js
+++ animation.js
@@ -1,17 +1,16 @@
 // Starting page animation //
 const logo = document.querySelector('.blog-logo');
-const title = document.querySelector('.name span');
 const contact = document.querySelector('#first-contact');
-const medias = document.querySelectorAll('.logo-border');
+const medias = document.querySelectorAll('.logo-btn');
 
 window.addEventListener('load', () => {
 
     const introTimeline = gsap.timeline({paused: true});
 
     introTimeline
-        .from(logo, 1, {transform: 'scale(0)', ease: 'power2.out'})
-        .from(contact, 1, {opacity:0, ease: "power2.out"}, '-=1')
-        .staggerFrom(medias, 1, {left: -200, ease: "power2.out"}, 0.3, '-=2');
+    .from(logo, 1, {transform: 'scale(0)', ease: 'power2.out'})
+    .from(contact, 1, {opacity:0, ease: "power2.out"})
+    .staggerFrom(medias, 1, {opacity:0, ease: "power2.out"}, 0.5)
 
     introTimeline.play();
 
@@ -23,15 +22,13 @@ const titleAnim = document.getElementById('txtAnim');
 function writer(){
 
     new Typewriter( titleAnim, { deleteSpeed: 10})
-    .changeDelay(20)
+    .changeDelay(50)
     .typeString("Hello, I'm Hugues, <br>")
     .pauseFor(300)
-    .typeString('<span style="color: #f4ac9f">Amateur Photographer !</span>')
+    .typeString('<span style="color: #f4ac9f">Amateur Photographer, </span><br>')
     .pauseFor(1000)
-    .deleteChars(22)
-    .typeString('<span style="color: #5c7457">Globe-trotter !</span>')
+    .typeString('<span style="color: #5c7457">Globe-trotter, </span><br>')
     .pauseFor(1000)
-    .deleteChars(15)
     .typeString('<span style="color: #7a79af">Dev Full-stack !</span>')
     .start();    
 }
